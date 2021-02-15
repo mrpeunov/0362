d288eb6c47ae19782653c27963983fb6ad6337b7
Gonza1es <chelion505@gmail.com>
2021-02-15 22:55:54 +0300

    Task1 donegit status!

---
+++ gnezdilov/task1/Eratofen.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("2\n3\n5\n7\n");
+	for (int i = 2; i < 101; i++)
+	{
+		if (i % 2 != 0 && i % 3 != 0 && i % 4 != 0 && i % 5 != 0 && i % 6 != 0 && i % 7 != 0 && i % 8 != 0 && i % 9 != 0 && i % 10 != 0)
+		{
+			printf("%d\n", i);
+		}
+	}
+}
+
