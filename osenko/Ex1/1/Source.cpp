480010c93338f72e0ba1767cb9fbd91f41b57037
TreFee <TreFee@users.noreply.github.com>
2021-02-28 19:23:25 +0300

    1 done

---
+++ osenko/Ex1/1/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int a = 55, b = 28, c = 2002, k = 36211;
+	cout << pow(round(sqrt(k)*1)/1 - 1, 2) << " <= " << k << " < " << pow(round(sqrt(k) * 1) / 1 + 1, 2);
+
+return 0;
+}
