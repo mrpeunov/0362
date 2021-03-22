f696b8c629b16c0e16cf9b9014f862e229965235
Vova-Tciulin <vtciulin@mail.ru>
2021-03-23 00:09:19 +0300

    Task4 donegit add -A!

---
+++ tciulin/task4/Source.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+
+using namespace::std;
+int proverka(int N, int *m)//проверяем делители на взаимно простые
+{
+	int b = 2;
+	for (int i = 0; i < N-1; i++)
+	{
+		for (int j = i+1; j < N; j++)
+		{
+			while ((b <= m[i])||(b<=m[j]))
+			{
+				if (((m[i] % b) == 0 )&& ((m[j] % b) == 0))
+					return 1;
+				else b++;
+			}
+			b = 2;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int N;
+	cout << "Китайская теорема об остатках!" << endl;
+	cout << "Введите кол-во чисел:";
+	cin >> N;
+	
+	int* a = new int[N];
+	int* m = new int[N];
+	int* mi = new int[N];
+	int* y = new int[N];
+	int X=0,M = 1, f = 0,z=1,prover;
+	//Вводим числа
+	TryAgain:
+	for (int i = 0; i < N; i++)
+	{
+
+		cout << "Введите остаток  a и делитель m:\n ";
+		cout << "a =";
+		cin >> a[i];
+		cout << " m =";
+		cin >> m[i];
+		
+		M = M * m[i];
+	}
+	//проверяем на корректность делители
+	if (proverka(N, m) == 1)
+	{
+		cout << "Вы ввели неверные делители(они не взаимно простые числа),попробуйте еще раз:\n";
+		goto TryAgain;
+	}
+	cout << "Вы ввели:\n";
+	//выводим изначальные данные и Находим M(0)
+	for (int i = 0; i < N; i++)
+	{
+		cout << "x = " << a[i] << " mod " << m[i] << endl;
+		mi[i] = M / m[i];
+	}
+	//для каждого выражения находим y(i)
+	for (int i = 0; i < N; i++)
+	{
+		if (mi[i] > m[i])
+			f = mi[i] / m[i];
+		y[i] = mi[i] - (f*m[i]);
+		while (((y[i]*z) % m[i] )!= a[i])
+		{
+			z++;
+		}
+		y[i] = z;
+		z = 1;
+		f = 0;
+	}
+	//считаем результат
+	for (int i = 0; i < N; i++)
+	{
+		X = X + mi[i] * y[i];
+	}
+	cout << "\nОтвет: ";
+	cout << "X = "<<X;
+	delete[]a;
+	delete[]m;
+	delete[]mi;
+	delete[]y;
+	return 0;
+}
