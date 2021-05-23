4faf550deadee01dd6828416d7cc83d2c8c26545
avvakumtheprotopope <78872231+avvakumtheprotopope@users.noreply.github.com>
2021-05-24 02:15:33 +0300

    Your ad could be here

--- jmarev/task7/main.cpp
+++ jmarev/task7/main.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <limits>
 
 using namespace std;
-
 
 struct node
 {
@@ -12,121 +12,149 @@ struct node
     int frequency;
     node* left;
     node* right;
+    bool flag;
 };
 
-/*struct cipher
+struct cipher
 {
     char symbol;
     int frequency;
     string code;
-};*/
+};
 
-bool comp(node a, node b) //comparator
+bool NodeComp(node a, node b) //comparator for node structure
 {
     return a.frequency < b.frequency;
 }
 
-void PrintInInfixOrder(node* current_node)
+bool CipherComp(cipher a, cipher b) //comparator for cipher structure
+{
+    return a.frequency < b.frequency;
+}
+
+void PostfixDelete(node* current_node,node* root)
 {
     if(current_node!=nullptr)
     {
-        PrintInInfixOrder(current_node->left);
-        printf("%c    %d\n",current_node->symbol,current_node->frequency);
-        PrintInInfixOrder(current_node->right);
-    };
+        PostfixDelete(current_node->left,root);
+        PostfixDelete(current_node->right,root);
+        if (current_node != root)
+        {
+            delete (current_node);
+        }
+    }
 }
 
 int main()
 {
-    node current_node;
-    node* leaf_one,* leaf_two;
-    //cipher* result;
+    node current_node{};
+    node* leaf_one,* leaf_two,* current_node_pointer;
+    cipher current_result;
+    vector <cipher> result;
     string message,encoded_message;
-    vector<node> alphabet;
+    vector <node> alphabet;
     char current_symbol;
-    int i=0,current_frequency;
-    //int t;
+    int i=0,t,current_frequency,size_var;
+
     do {
         cin>>current_symbol;
-        if(current_symbol=='0')
+        if(current_symbol == '0')
         {
             break;
         }
-        cin>>current_frequency;
-        alphabet.push_back(current_node);
-        alphabet[i].symbol=current_symbol;
-        alphabet[i].frequency=current_frequency;
-        alphabet[i].left=alphabet[i].right=nullptr;
-        i++;
-    }while(current_symbol!='0');
-    /*result=new cipher[alphabet.size()];
-    sort(alphabet.begin(),alphabet.end(),comp);
-    for(i=0;i<alphabet.size();i++)
-    {
-        result[i].symbol=alphabet[i].symbol;
-        result[i].frequency=alphabet[i].frequency;
-    }*/
 
-    while(alphabet.size()>1)
+        cin>>current_frequency;
+
+        alphabet.push_back(current_node);
+
+        alphabet[i].symbol = current_symbol;
+        alphabet[i].flag = 0;
+        alphabet[i].frequency = current_frequency;
+        alphabet[i].left = alphabet[i].right=nullptr;
+        i++;
+
+    }while(current_symbol!='0');
+
+    sort(alphabet.begin(),alphabet.end(),NodeComp);
+    size_var = alphabet.size();
+    result.resize(alphabet.size());
+
+    while(alphabet.size() > 1)
     {
-        sort(alphabet.begin(),alphabet.end(),comp);
-        printf("Next iteration:\n");
-        for(i=0;i < alphabet.size();i++)
-        {
-            printf("%c    %d\n",alphabet[i].symbol,alphabet[i].frequency);
-        }
-        /*for (i=0;i<sizeof(result);i++)
-        {
-            cout<<result[i].code<<"\n";
-        }*/
+        sort(alphabet.begin(),alphabet.end(),NodeComp);
 
         printf("\n");
 
         leaf_one=new node;
         leaf_two=new node;
 
-        (*leaf_one)=alphabet[0];
-        /*i=0;
-        while(result[i].symbol!=alphabet[0].symbol)
-        {
-        i++;
-        }
-        result[i].code="1"+result[i].code;*/
-        (*leaf_two)=alphabet[1];
-        /*i=0;
-        while(result[i].symbol!=alphabet[1].symbol)
-        {
-            i++;
-        }
-        result[i].code="0"+result[i].code;*/
-        alphabet.erase(alphabet.begin()); //erase one of the leafs from vector
+        (*leaf_one) = alphabet[0];
+        (*leaf_two) = alphabet[1];
 
-        alphabet[0].frequency=leaf_one->frequency+leaf_two->frequency; // fill the fields of other leaf with parent' values
-        alphabet[0].left=leaf_one;
-        alphabet[0].right=leaf_two;
-        alphabet[0].symbol='$';       //random technical value, doesnt matter at all
-    }
-    printf("Next iteration:\n");
-    printf("%c    %d\n",alphabet[0].symbol,alphabet[0].frequency);
-    printf("\n");
-    printf("Infix tree print:\n");
-    PrintInInfixOrder(&alphabet[0]);     //infix printf of the tree with algorithm iterations
+        alphabet.erase(alphabet.begin()); //erase one of the leaves from vector
 
-    /*for (i=0;i<sizeof(result);i++)
-    {
-        printf("%c   %d   ",result[i].symbol,result[i].frequency);
-        cout<<result[i].code<<"\n";
+        alphabet[0].frequency = leaf_one->frequency+leaf_two->frequency; // fill the fields of other leaf with parent' values
+        alphabet[0].left = leaf_one;
+        alphabet[0].right = leaf_two;
+        alphabet[0].symbol = '$';       //random technical value, doesnt matter at all
     }
-    cin>>message;         //scan message without spaces
-    for(i=0;i<message.size();i++)
+
+    for(i=0;i<size_var;i++)
     {
-        t=0;
-        while(message[i]!=result[t].symbol)
+        current_node_pointer=&alphabet[0];
+
+        do
         {
-            t++;
+            if(current_node_pointer->right->flag && current_node_pointer->left->flag)
+            {
+                result[i].code.clear();
+                i --;
+                break;
+            }
+            else if(!current_node_pointer->right->flag)
+            {
+                current_node_pointer = current_node_pointer->right;
+                result[i].code.insert(result[i].code.end(),'0');
+                continue;
+            }
+            else if(!current_node_pointer->left->flag)
+            {
+                current_node_pointer = current_node_pointer->left;
+                result[i].code.insert(result[i].code.end(),'1');
+                continue;
+            }
+
+        }while(current_node_pointer->left != nullptr);//if one of the links(i've chosen to check the lef one) is equal to NULL,
+        current_node_pointer->flag = true;              //it means that both links are equal to NULL and it's leaf of the tree
+        if(current_node_pointer->symbol != '$')
+        {
+            result[i].symbol = current_node_pointer->symbol;
+            result[i].frequency = current_node_pointer->frequency;
         }
-        encoded_message=encoded_message+result[t].code;
     }
-    cout<<encoded_message;*/            //print encoded message
+
+    PostfixDelete(&alphabet[0],&alphabet[0]);
+    alphabet.clear();
+    sort(result.begin(),result.end(),CipherComp);
+    for(i = 0;i < size_var;i ++)
+    {
+        cout << result[i].symbol << "   " << result[i].code << endl;
+    }
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin,message);       //scan message
+    cout << message << endl;
+    for(i = 0;i < message.size();i ++)
+    {
+        t = 0;
+        while(message[i] != result[t].symbol)
+        {
+            t ++;
+        }
+        encoded_message.insert(encoded_message.size(),result[t].code);
+    }
+    result.clear();
+    cout << encoded_message;            //print encoded message
     return 0;
 }
